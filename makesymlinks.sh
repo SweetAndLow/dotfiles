d365b0e131c16ad3477e2ea70363e08b39205005
SweetAndLow <vetters10@msn.com>
2014-05-15 14:19:52 -0700

    added keepasshttp and install_keepass function
    makesymlink now installs keepass and symlinks the dll's correctly.
    Updates to syntastic have been pulled in and keepasshttp submodule
    added.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,65 +1,66 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles
+# in ~/dotfiles. It also can install keepass and configure chromeIPass.
 ############################
 
 ########## Variables
 
-dir=$HOME/dotfiles                    # dotfiles directory
-olddir=$HOME/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim bash_custom bash_isilon"    # list of files/folders to symlink in homedir
+dir=$HOME/dotfiles # dotfiles directory
+olddir=$HOME/dotfiles_old # old dotfiles backup directory
+files="bashrc vimrc vim bash_custom bash_isilon" # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd "$dir"
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create
+# symlinks from the homedir to any files in the ~/dotfiles directory specified
+# in $files
 for file in $files; do
-    if [ -L $HOME/.$file ]; then # check for symbolic link first
+    if [ -L "$HOME/.$file" ]; then # check for symbolic link first
         echo "unlinking .$file"
-        unlink $HOME/.$file
-    elif [ -f $HOME/.$file ]; then
+        unlink "$HOME/.$file"
+    elif [ -f "$HOME/.$file" -o -d "$HOME/.$file" ]; then
         echo "Moving any existing dotfiles from $HOME to $olddir"
-        mv $HOME/.$file $HOME/dotfiles_old/
+        mv "$HOME/.$file" "$HOME/dotfiles_old/"
     fi
 
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file $HOME/.$file
+    ln -s "$dir/$file" "$HOME/.$file"
 done
 
-#install_zsh () {
-## Test to see if zshell is installed.  If it is:
-#if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-#    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-#    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-#        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
-#    fi
-#    # Set the default shell to zsh if it isn't currently set to zsh
-#    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-#        chsh -s $(which zsh)
-#    fi
-#else
-#    # If zsh isn't installed, get the platform of the current machine
-#    platform=$(uname);
-#    # If the platform is Linux, try an apt-get to install zsh and then recurse
-#    if [[ $platform == 'Linux' ]]; then
-#        sudo apt-get install zsh
-#        install_zsh
-#    # If the platform is OS X, tell the user to install zsh :)
-#    elif [[ $platform == 'Darwin' ]]; then
-#        echo "Please install zsh, then re-run this script!"
-#        exit
-#    fi
-#fi
-#}
+# Installs keepass2 and mono-complete. Also configures symlinks for the .dll's
+# that are requried to get chromeIPass working in Linux.
+install_keepass2() {
+    keepass_files="Newtonsoft.Json.dll KeePassHttp.dll"
+    keepass_lib="/usr/lib/keepass2"
+    # Test to see if keepass2 is installed
+    if [ ! -d "$keepass_lib" ]; then
+        echo "keepass2 isn't installed, installing..."
+        platform=$(uname);
+        if [ "$platform" == 'Linux' ]; then
+            sudo apt-get install -y keepass2 mono-complete
+        fi
+    fi
 
-#install_zsh
+    for file in $keepass_files; do
+        if [ -L "$keepass_lib/$file" ]; then
+            echo "unlinking $file"
+            sudo unlink "$keepass_lib/$file"
+        fi
+        echo "Creating symlink: $file in $keepass_lib"
+        sudo ln -s "$dir/submodules/keepasshttp/mono/$file" "$keepass_lib/$file"
+    done
+}
+
+install_keepass2
