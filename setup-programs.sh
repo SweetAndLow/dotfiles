104731ca851b62d8a95bc45a3cc0ac2852df9649
SweetAndLow <vetters10@msn.com>
2014-06-24 13:53:28 -0700

    script/doc that tracks my installed applications

---
+++ setup-programs.sh
@@ -0,0 +1,23 @@
+# Author: Sean Vetter
+# Date: 6/20/2014
+# Description: This is a collection of programs and repos that i use to setup
+# a new ubuntu computer. This isn't a script just yet.
+
+sudo apt-get -y install git subversion vim pylint nfs-common cifs-utils ssh \
+    chromium-browser pepperflashplugin-nonfree djview-plugin keepass2 \
+    nautilus-dropbox traceroute mono-complete screen whois irssi rdesktop \
+    python-setuptools curl
+
+
+# PPA
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+#GIT core
+sudo add-apt-repository ppa:git-core/ppa
+# SVN
+sudo add-apt-repository ppa:svn/ppa
+
+# review board tools install, requires python-setuptools
+sudo easy_install -U RBTools
